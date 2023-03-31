a6df8bb3fce5e3310edf3a56e654ad30b7dd1435
mirzoxid <mirzoxidadxamjanov01@gmail.com>
2023-03-31 17:25:59 +0500

    translation

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: [ 
                 ...refreshPaths,
                 'app/Http/Livewire/**',
                 'app/Forms/Components/**',
-            ],
+            ], 
         }),
     ],
 })
